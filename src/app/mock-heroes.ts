b1c8392d7542b3e1ccd8acfcfd3d2e2c647d7104
SADEESHA <pererasadeesha1@gmail.com>
2020-04-28 22:36:08 +0530

    TourOfHeroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from './hero';
+export const HEROES: Hero[]=[
+    {id:11, name:'Dr. Nice'},
+    {id:12, name:'Narco'},
+    {id:13, name:'Bombasto'},
+    {id:14, name:'Celeritas'},
+    {id:15, name:'Magneto'},
+    {id:16, name:'SpiderMan'},
+    {id:17, name:'TheFlash'},
+    {id:18, name:'Dr. Strange'},
+    {id:19, name:'Thor'},
+    {id:20, name:'TheHulk'}
+];
